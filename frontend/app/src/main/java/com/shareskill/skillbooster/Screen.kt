946a6b971cd19cb318b3ff24d4113aee0568524b
Bethlehem Woldekidan <nate85here@gmail.com>
2025-05-08 20:13:40 -0400

    Delete frontend directory

--- frontend/app/src/main/java/com/shareskill/skillbooster/Screen.kt
+++
@@ -1,9 +0,0 @@
-package com.shareskill.skillbooster
-
-sealed class Screen(val route: String) {
-    object Landing : Screen("landing")
-    object RoleSelection : Screen("role_selection")
-    object StudentHome : Screen("student_home")
-    object TeacherHome : Screen("teacher_home")
-
-}
