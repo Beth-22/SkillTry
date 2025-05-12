b71b99f6dfd455b5d2308316ee3dfb02d7ed69f8
Bethlehem Woldekidan <nate85here@gmail.com>
2025-05-12 14:15:27 -0400

    Delete front_end directory

--- front_end/app/src/main/java/com/example/skillboost/models/CourseStatus.kt
+++
@@ -1,8 +0,0 @@
-package com.example.skillboost.models
-
-data class CourseStatus(
-    val title: String,
-    val description: String,
-    val status: String,
-    val imageRes: Int
-)
--- front_end/app/src/main/java/com/example/skillboost/models/User.kt
+++
@@ -1,8 +0,0 @@
-package com.example.skillboost.model
-
-data class User(
-    val id: String,
-    val email: String,
-    val password: String,
-    val isSignedIn: Boolean = false
-)
